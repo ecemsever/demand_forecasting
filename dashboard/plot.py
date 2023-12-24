58ca3a36213f30ff13229be9480ec5e55706e3f4
unknown <ecemmsever@gmail.com>
2023-12-24 12:09:43 +0300

    initial

---
+++ dashboard/plot.py
@@ -0,0 +1,266 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import os
+import datetime
+from PIL import Image
+
+dir = os.path.dirname(__file__)
+
+st.set_page_config(
+    page_title="Welcome to PowerRangers Dashboard",
+    page_icon="👋",
+    layout='wide'
+    )
+
+with st.sidebar:
+    red = Image.open(dir+'/images/red.png')
+    yellow = Image.open(dir+'/images/yellow.png')
+    green = Image.open(dir+'/images/green.png')
+    black = Image.open(dir+'/images/black.png')
+    st.markdown('<h1><center> 📬 Contact Us </center></h1>', unsafe_allow_html=True)
+    st.write("##")
+
+    with st.container():
+
+        col1, col2 = st.columns(2)
+
+        col1.image(red)
+        col1.link_button("Muratcan", "https://www.linkedin.com/in/muratcankaplan?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADye9q4BjqgGfVqmNgKQS-HhQalOfJZBKyU&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BXhxGWeQVQZWMxiKi0iy1xg%3D%3D", use_container_width=True)
+
+        col2.image(yellow)
+        #col2.markdown("[Ecem Savaş](https://www.linkedin.com/in/ecemsever-bilkent/)")
+        col2.link_button("Ecem", "https://www.linkedin.com/in/ecemsever-bilkent/", use_container_width=True)
+
+    with st.container():
+
+        col1, col2 = st.columns(2)
+        col1.image(green)
+        col1.link_button("Ahmet Emre", "https://www.linkedin.com/in/ahmet-emre-topba%C5%9F-267391121?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAB4iVS8BN9tfUXZo2j0AcuCVnR603hSF_4A&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BleRDOKiKSZSdvj%2BtTawywA%3D%3D", use_container_width=True)
+
+        col2.image(black)
+        col2.link_button("Gürkan", "https://www.linkedin.com/in/g%C3%BCrkan-g%C3%BCnd%C3%BCz-970753193?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAAC1-sqQBNxIm1OVJyqtMhIZrD9Gs8PIj-yU&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BktNP0zL%2BSuqRXnutJ5IYEg%3D%3D", use_container_width=True)
+
+
+# a little trick to shorten & center logo
+col1, col2 ,col3 = st.columns(3)
+
+powerrangers = Image.open(dir+'/images/powerrangers.png')
+col2.image(powerrangers)
+
+st.markdown("<h3> <p><center> Energy Analytics for Victoria, Australia </center></p> </h3>", unsafe_allow_html=True)
+
+def history():
+    history_data = pd.read_csv(dir+'/historical_demand.csv')    
+    date_col = 'timestamp'
+    # Construct date granularity features for past data
+    history_data['Day'] = pd.to_datetime(history_data[date_col]).dt.date
+    history_data['Year'] = pd.to_datetime(history_data[date_col]).dt.year
+    history_data['Month'] = pd.to_datetime(history_data[date_col]).dt.month
+    history_data['Week'] = pd.to_datetime(history_data[date_col]).dt.strftime('%W')
+    history_data[['Month']]=history_data[['Month']].astype(str).apply(lambda x: x.str.zfill(2))
+    history_data[['Week']]=history_data[['Week']].astype(str).apply(lambda x: x.str.zfill(2))
+    history_data['Year/Month'] = history_data['Year'].astype(str) + '/' + history_data.Month.astype(str)
+    history_data['Year/Week'] = history_data['Year'].astype(str) + '/' + history_data.Week.astype(str)
+    history_data = history_data.rename(columns={"timestamp": "Date"})
+    return history_data
+
+def data_8_hours():
+    pred_8_hours = pd.read_csv(dir+'/8_hour_predictions.csv')    
+    pred_8_hours=pred_8_hours.rename(columns={"timestamp": "Date"})
+
+    return pred_8_hours
+
+def data_168_hours():
+    pred_168_hours = pd.read_csv(dir+'/168_hour_predictions.csv')    
+    pred_168_hours=pred_168_hours.rename(columns={"timestamp": "Date"})
+    return pred_168_hours
+
+history_data = history()
+pred_8_hours = data_8_hours()
+pred_168_hours = data_168_hours()
+
+# Mape calculation function
+def calculate_mape(actual, predicted):
+    return (1 / len(actual)) * sum(abs((actual - predicted) / actual) * 100)
+
+# Function for converting to csv for download
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+# Processing prediction data to prepare it for the mape calculation
+# Get the latest 8 values in the data and flatten 8 predictions that corresponds to that actual values
+def t_minus_1_data_process(df, num_hours): 
+    # get latest 8 values
+    t_minus_1_all = df.iloc[(len(df)-(num_hours+1)):(len(df)+1)]
+    # hold actual values in one dataframe
+    t_minus_1_actual = t_minus_1_all[['Date','total_demand']].iloc[1:(num_hours+1)]
+    # flatten 8 predictions that corresponds to that actual values 
+    t_minus_1_pred = pd.DataFrame(t_minus_1_all.iloc[0,2:(num_hours+2)]).reset_index(drop=True)
+    t_minus_1_pred = t_minus_1_pred.rename(columns={t_minus_1_pred.columns[-1] : "demand_prediction"})
+    # merge actual and predicted values
+    t_minus_1 = pd.concat([t_minus_1_actual.reset_index(drop=True), t_minus_1_pred.reset_index(drop=True)], axis=1)
+    t_minus_1 = pd.DataFrame(t_minus_1)
+    t_minus_1['demand_prediction'] = t_minus_1['demand_prediction'].astype(float)
+
+    return t_minus_1
+    # Preparing data for the prediction plot
+def pred_data_process(df, num_hours):
+    latest_pred = pd.DataFrame(df.iloc[-1,2:(num_hours+2)]).reset_index(drop=True)
+    latest_pred = latest_pred.rename(columns={latest_pred.columns[-1] : "demand_prediction"})
+    latest_pred_date = df['Date'].iloc[-1]
+    pred_dates = pd.date_range(start=(pd.to_datetime(latest_pred_date)+ pd.Timedelta(hours=1)), end=(pd.to_datetime(latest_pred_date) + pd.Timedelta(hours=num_hours)),freq='H')
+    pred_w_dates = pd.concat([latest_pred.reset_index(drop=True), pd.DataFrame(pred_dates).reset_index(drop=True)], axis=1).rename(columns={0 : "Date", "demand_prediction" : "Prediction"})
+
+    return pred_w_dates
+
+# Name the tabs
+tab1, tab2, tab3 = st.tabs(["🌍 Historical Analytics", "📈 Shift Based Predictive Analytics", "📊 Weekly Predictive Analytics"])
+
+with tab1:
+    
+    history_data_daily = history_data.groupby(['Day'])['total_demand'].agg('sum').reset_index(name='Total Demand')
+
+
+    col1, col2 = st.columns(2)
+    #Default start date is the minimum date in the data
+    start_date = min(history_data_daily["Day"])
+    
+    #Start Date filter
+    sd = col1.date_input("📆 Select a starting date",start_date)
+
+    #Default end date is the maximum date in the data
+    end_date = max(history_data_daily["Day"])
+
+    # End date filter
+    ed = col2.date_input("📆 Select a end date",end_date)
+
+    # Time granularity selectbox. 
+    with col1:
+        option = st.selectbox(
+        'Please select a time granularity',
+        ('Hourly', 'Daily', 'Weekly','Monthly'))
+
+    # Depending on the selection, graph is updated
+    if option=='Daily' :
+        line_graph = px.line(
+            data_frame = history_data_daily[history_data_daily["Day"].between(sd, ed)], title='Total Demand Daily', 
+            x='Day', y='Total Demand')
+        st.plotly_chart(line_graph,use_container_width = True)
+        
+    elif option == 'Hourly':
+        # Create the line graph for hourly
+        line_graph2 = px.line(
+            data_frame = history_data[history_data["Day"].between(sd, ed)], title='Total Demand Hourly', 
+            x='Day', y='total_demand',
+            hover_data = ["Date"]
+            )
+        st.plotly_chart(line_graph2,use_container_width = True)
+        
+    elif option == 'Weekly' :
+        pred_8_hours_weekly = history_data[history_data["Day"].between(sd, ed)].groupby(['Year/Week'])['total_demand'].agg('sum').reset_index(name='Total Demand')
+        # Create the line graph for weekly
+        line_graph4 = px.line(
+             data_frame = pred_8_hours_weekly, title='Total Demand Weekly', 
+             x='Year/Week', y='Total Demand')
+        st.plotly_chart(line_graph4,use_container_width = True)
+
+    else:
+        pred_8_hours_monthly = history_data[history_data["Day"].between(sd, ed)].groupby(['Year/Month'])['total_demand'].agg('sum').reset_index(name='Total Demand')
+        # Create the line graph for monthly
+        line_graph3 = px.line(
+            data_frame = pred_8_hours_monthly, title='Total Demand Monthly', 
+            x='Year/Month', y='Total Demand')
+        st.plotly_chart(line_graph3,use_container_width = True)
+
+
+with tab2:
+    
+    t_minus_1= t_minus_1_data_process(pred_8_hours, 8)
+    pred_w_dates = pred_data_process(pred_8_hours, 8)
+    
+    # Download predictions
+    csv = convert_df(pred_w_dates)
+    st.download_button(
+        label="Download Predictions as CSV",
+        data=csv,
+        file_name='predictions_8hours.csv',
+        mime='text/csv')
+
+    if st.button('Update the 8 hours Data', type="primary"):
+        pred_8_hours = data_8_hours()
+        t_minus_1= t_minus_1_data_process(pred_8_hours, 8)
+        pred_w_dates = pred_data_process(pred_8_hours, 8)
+
+    mape_8 = calculate_mape(t_minus_1['total_demand'],t_minus_1['demand_prediction'])
+
+    # Publishing the first row
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Prediction Available till", str(pd.to_datetime(pred_8_hours['Date'].iloc[-1]) + pd.Timedelta(hours=8)), "")
+    col2.metric("Last Update", str(pd.Timestamp.today().strftime('%Y-%m-%d')) , "", delta_color="off")
+    col3.metric("MAPE", str(round(mape_8,2))+"%")
+
+    # Actual vs Prediction Graph
+    line_graph0 = px.line(
+        data_frame = t_minus_1, title = 'Actual vs Predicted Hourly Demand for the last 8-hour period', 
+        x='Date', y = ['total_demand','demand_prediction'],
+        hover_data = ["Date"],
+        labels = {"timestamp": "Date","variable":""},
+        color_discrete_sequence = ['red', 'blue']
+        )
+    new = {"demand_prediction": "Prediction","total_demand": "Actual Demand"}
+    line_graph0.for_each_trace(lambda t: t.update(name = new[t.name]))
+    line_graph0.update_layout(yaxis=dict(range = [6000, max(t_minus_1['total_demand']*1.3)]))
+    st.plotly_chart(line_graph0,use_container_width = True)
+  
+    # Prediction Graph for the next 8 hours
+    line_graph_pred = px.line(
+        data_frame = pred_w_dates, title='Next 8 hours Hourly Predictions', 
+        x='Date', y='Prediction',
+        )
+    line_graph_pred.update_layout(yaxis=dict(range=[6000, max(pred_w_dates['Prediction']*1.3)]))
+    st.plotly_chart(line_graph_pred,use_container_width = True)
+
+with tab3:
+    t_minus_1_168hours= t_minus_1_data_process(pred_168_hours, 168)
+    pred_w_dates_168hours = pred_data_process(pred_168_hours, 168)
+    
+    # Download predictions
+    csv = convert_df(pred_w_dates_168hours)
+    st.download_button(
+        label="Download Predictions as CSV",
+        data=csv,
+        file_name='predictions_168hours.csv',
+        mime='text/csv')
+
+    if st.button('Update the Weekly Data', type="primary"):
+        pred_168_hours = data_168_hours()
+        t_minus_1_168hours= t_minus_1_data_process(pred_168_hours, 168)
+        pred_w_dates_168hours = pred_data_process(pred_168_hours, 168)
+
+    mape_168 = calculate_mape(t_minus_1_168hours['total_demand'],t_minus_1_168hours['demand_prediction'])
+
+    # Publishing the first row
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Prediction Available till", str(pd.to_datetime(pred_168_hours['Date'].iloc[-1]) + pd.Timedelta(hours=168)), "")
+    col2.metric("Last Update", str(pd.Timestamp.today().strftime('%Y-%m-%d')) , "", delta_color="off")
+    col3.metric("MAPE", str(round(mape_168,2))+"%")
+
+    # Actual vs Prediction Graph
+    line_graph168 = px.line(
+        data_frame = t_minus_1_168hours, title='Actual vs Predicted Hourly Demand for the last 168-hour period', 
+        x='Date', y=['total_demand','demand_prediction'],
+        hover_data=["Date"],
+        labels={"timestamp": "Date","variable":""},
+        color_discrete_sequence=['red', 'blue']
+        )
+    new = {"demand_prediction": "Prediction","total_demand": "Actual Demand"}
+    line_graph168.for_each_trace(lambda t: t.update(name = new[t.name]))
+    st.plotly_chart(line_graph168,use_container_width = True)
+
+    # Prediction Graph for the next 168 hours
+    line_graph_pred168 = px.line(
+        data_frame = pred_w_dates_168hours, title='Next 168 hours Hourly Predictions', 
+        x='Date', y='Prediction',
+        )
+    st.plotly_chart(line_graph_pred168,use_container_width = True)
---
+++ pipeline/main.py
@@ -0,0 +1,212 @@
+import pandas as pd
+import numpy as np
+# import matplotlib.pyplot as plt
+import os
+import pickle
+import time
+# import pandas as pd
+# import numpy as np
+# from datetime import datetime
+# import math
+# import pandas as pd
+import xgboost as xgb
+# import wandb
+# from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
+# import matplotlib.pyplot as plt
+# from sklearn.model_selection import train_test_split
+# import pandas as pd
+# import numpy as np
+
+# from threading import Timer
+# from datetime import datetime, timedelta
+
+# print(os.path.abspath(os.curdir))
+# os.chdir("app")
+# print(os.path.abspath(os.curdir))
+
+if os.path.exists("8_hour_predictions.csv"):
+     os.remove("8_hour_predictions.csv")
+if os.path.exists("168_hour_predictions.csv"):
+     os.remove("168_hour_predictions.csv")
+
+special_holidays = [
+    "1-1",  # New Year's Day
+    "2-1",  # New Year's Day (additional day)
+    "26-1",  # Australia Day
+    "13-3",  # Labour Day
+    "7-4",  # Good Friday
+    "8-4",  # Easter Saturday
+    # Since you did not provide a date for "Easter Sunday", I'm omitting it.
+    "10-4",  # Easter Monday
+    "25-4",  # Anzac Day
+    "12-6",  # King's Birthday
+    # Omitting the AFL Grand Final date since it's TBC.
+    "7-11",  # Melbourne Cup
+    "25-12",  # Christmas Day
+    "26-12"  # Boxing Day
+]
+
+
+# Helper function to determine the season based on the month
+def get_season(month):
+    if 12 <= month <= 2:
+        return 'summer'
+    elif 3 <= month <= 5:
+        return 'autumn'
+    elif 6 <= month <= 8:
+        return 'winter'
+    elif 9 <= month <= 11:
+        return 'spring'
+
+def periodic_encoding(x, max_val):
+    # Calculate sine and cosine values and explicitly cast them as float
+    sin = np.sin(2 * np.pi * x / max_val).astype('float64')
+    cos = np.cos(2 * np.pi * x / max_val).astype('float64')
+    return sin, cos
+
+def create_forecasting_dataset(df, n_lag, n_next, special_holidays, 
+                               day_embedding='dummy', week_embedding='dummy', season_embedding='dummy'):
+    # Convert valid_start to datetime and extract components
+    df['valid_start'] = pd.to_datetime(df['valid_start'])
+    df['hour'] = df['valid_start'].dt.hour
+    df['day'] = df['valid_start'].dt.day
+    df['month'] = df['valid_start'].dt.month
+    df['year'] = df['valid_start'].dt.year
+    df['day_of_week'] = df['valid_start'].dt.dayofweek
+    df['week_of_year'] = df['valid_start'].dt.isocalendar().week.astype('float64')
+    df['season'] = df['month'].apply(get_season)
+    
+    # Identify special holidays
+    df['date_str'] = df['valid_start'].dt.strftime('%d-%m')
+    df['is_special_day'] = df['date_str'].isin(special_holidays).astype(int)
+    
+    # Periodic or Dummy Encoding
+    if day_embedding == 'periodic':
+        day_sin, day_cos = periodic_encoding(df['day_of_week'], 7)
+        df['day_sin'] = day_sin
+        df['day_cos'] = day_cos
+    else:
+        df = pd.get_dummies(df, columns=['day_of_week'])
+
+    if week_embedding == 'periodic':
+        # Encode the week_of_year column
+        week_sin, week_cos = periodic_encoding(df['week_of_year'], 52)
+        
+        # Assign the encoded values ensuring they're in the correct float format
+        df['week_sin'] = week_sin.astype('float64')
+        df['week_cos'] = week_cos.astype('float64')
+    else:
+        # If not 'periodic', apply dummy encoding
+        df = pd.get_dummies(df, columns=['week_of_year'])
+
+    if season_embedding == 'periodic':
+        seasons_map = {'spring': 0, 'summer': 1, 'autumn': 2, 'winter': 3}
+        df['season'] = df['season'].map(seasons_map)
+        season_sin, season_cos = periodic_encoding(df['season'], 4)
+        df['season_sin'] = season_sin
+        df['season_cos'] = season_cos
+    else:
+        df = pd.get_dummies(df, columns=['season'])
+
+    # Create lagged demand values
+    for lag in range(1, n_lag + 1):
+        df[f'lag_{lag}_hours'] = df['total_demand'].shift(lag)
+
+    # Create future demand values
+    future_demands = [df['total_demand'].shift(-i) for i in range(n_next)]
+    df_future = pd.concat(future_demands, axis=1)
+    
+    # Ensure no NaN values in future demands and lagged features
+    mask = df_future.notna().all(axis=1) & df[[f'lag_{lag}_hours' for lag in range(1, n_lag + 1)]].notna().all(axis=1)
+    df = df.loc[mask]
+    df_future = df_future.loc[mask]
+
+    # Drop columns not needed for features, except for 'valid_start'
+    columns_to_drop = ['year', 'month', 'day', 'hour', 'date_str', 'total_demand']
+    if 'season_mapped' in df.columns:
+        columns_to_drop.append('season_mapped')
+    df.drop(columns=columns_to_drop, axis=1, inplace=True)
+    
+    # Convert object columns to float
+    for col in df.select_dtypes(include=['object']).columns:
+        df[col] = df[col].astype(float)
+    
+    # Ensure all floating point numbers are in the correct format
+    float_cols = df.select_dtypes(include=['float64', 'float32']).columns
+    df[float_cols] = df[float_cols].astype('float64')
+
+    # X includes 'valid_start' and other features, Y is future demands
+    X = df
+    Y = df_future.values
+    # Ensure that the features (X) are returned as a DataFrame
+    X = pd.DataFrame(X)
+    # The future values (Y) should also be a DataFrame if you're going to use .iloc on it
+    Y = pd.DataFrame(Y)
+
+    return X, Y
+
+# Load models
+with open('xgboost_model.pkl', 'rb') as file:
+    model_8 = pickle.load(file)
+
+with open('xgboost_model_168_v2.pkl', 'rb') as file:
+    model_168 = pickle.load(file)
+# with open('xgboost_model_168_v2.pkl', 'wb') as file:
+#     pickle.dump(model_168, file)
+# Load datasets
+train_df = pd.read_csv('train_dataset.csv')
+test_df = pd.read_csv('test_dataset.csv')
+
+
+def update_prediction_files(index, n_lag, train_df, test_df, model_8, model_168, filename_8, filename_168):
+    # Include all of the training data and the part of the test data up to the current index
+    if index < n_lag:
+        df_temp = pd.concat([train_df, test_df.iloc[:index]])
+    else:
+        df_temp = pd.concat([train_df, test_df.iloc[index - n_lag:index]])
+
+    # Process the combined data for 8-hour prediction
+    X_8, Y_8 = create_forecasting_dataset(df_temp, 168, 8, special_holidays, 'dummy', 'dummy', 'periodic')
+    predictions_8 = model_8.predict(X_8.iloc[-1:].drop(columns=['valid_start']))
+
+    # Process the combined data for 168-hour prediction
+    X_168, Y_168 = create_forecasting_dataset(df_temp, 168*6, 168, special_holidays, 'dummy', 'dummy', 'periodic')
+    predictions_168 = model_168.predict(X_168.iloc[-1:].drop(columns=['valid_start']))
+
+    # Extract the actual demand for the current hour from test_df
+    # actual_demand = df_temp.iloc[-1,5]
+    actual_demand_8 = (np.array(Y_8.iloc[-1:]).reshape(-1))[0]
+    actual_demand_168 = (np.array(Y_168.iloc[-1:]).reshape(-1))[0]
+    # print()
+    
+    # Write to CSV
+    timestamp = test_df.iloc[index]['valid_start']  # Timestamp from the current test row
+    
+    # Writing for 8-hour predictions
+    with open(filename_8, 'a') as f_8:
+        header_8 = 'timestamp,' + ','.join(['total_demand'] + [f'prediction_{i+1}' for i in range(8)])
+        f_8.write(f"{header_8}\n") if index == 0 else None
+        row_8 = f"{timestamp},{actual_demand_8},{','.join(map(str, predictions_8.flatten()))}"
+        f_8.write(f"{row_8}\n")
+
+    # Writing for 168-hour predictions
+    with open(filename_168, 'a') as f_168:
+        header_168 = 'timestamp,' + ','.join(['total_demand'] + [f'prediction_{i+1}' for i in range(168)])
+        f_168.write(f"{header_168}\n") if index == 0 else None
+        row_168 = f"{timestamp},{actual_demand_168},{','.join(map(str, predictions_168.flatten()))}"
+        f_168.write(f"{row_168}\n")
+
+
+# Function to run the update process at specified intervals
+def run_update_process(interval, train_df, test_df, model_8, model_168, filename_8, filename_168):
+    for index in range(len(test_df)):
+        update_prediction_files(index, 168*6, train_df, test_df, model_8, model_168, filename_8, filename_168)
+        # if index >168:
+        #     time.sleep(15)
+        # break;
+        time.sleep(interval)  # Wait for the specified interval
+        
+
+
+# Start the update process
+run_update_process(0, train_df, test_df, model_8, model_168, '8_hour_predictions.csv', '168_hour_predictions.csv')
